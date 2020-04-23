b38b41769947500ab8d111a9fab8ab1e8954abdd
Jack Cooper <jackcooper1245@gmail.com>
2020-04-23 10:01:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,19 +7,19 @@ def initialize(side_1, side_2, side_3)
 end
 
 
-def kind
-  if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-    raise TriangleError
-  elsif(@side_1 + @side_2) < @side_3 || (@side_1 + @side_3) < @side_1 || (@side_2 + @side_3) < @side_1
-    raise TriangleError
-  elsif (@side_1 == @side_2) && (@side_2 == @side_3)
-    return :equilateral
-  elsif (@side_1 == @side_2) && (@side_1 !== @side_3)
-    return :isosceles
-  elsif (@side_1 !== @side_2) && (@side_1 !== @side_3) && (@side_2 !== @side_3)
-    return :scalene
-end
-end
+#def kind
+#  if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+#    raise TriangleError
+#  elsif(@side_1 + @side_2) < @side_3 || (@side_1 + @side_3) < @side_1 || (@side_2 + @side_3) < @side_1
+#    raise TriangleError
+#  elsif (@side_1 == @side_2) && (@side_2 == @side_3)
+#    return :equilateral
+#  elsif (@side_1 == @side_2) && (@side_1 !== @side_3)
+#    return :isosceles
+#  elsif (@side_1 !== @side_2) && (@side_1 !== @side_3) && (@side_2 !== @side_3)
+#    return :scalene
+#end
+#end
 
 end
 
