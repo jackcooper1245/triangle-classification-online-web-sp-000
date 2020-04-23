a1b000932ad57cf93c7cec425520aa169f06a465
Jack Cooper <jackcooper1245@gmail.com>
2020-04-23 09:45:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,13 @@ end
 
 
 def kind
+  if @side_1 + @side_2 < @side_3 || @side_1 = @side_3 < @side_1 || @side_2 = @side_3 < @side_1
   if @side_1 == @side_2 && @side_1 == @side_3
     return :equilateral
   elsif @side_1 == @side_2 && @side_1 !== @side_3
     return :isosceles
   elsif @side_1 !== @side_2 && @side_1 !== @side_3 && @side_2 !== @side_3
+    return :scalene
 end
 
 end
