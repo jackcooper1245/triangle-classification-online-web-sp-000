7e1fdd6bc6e069fa0b60a540b66435ecfe83ebb4
Jack Cooper <jackcooper1245@gmail.com>
2020-04-23 09:52:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ def kind
   elsif @side_1 !== @side_2 && @side_1 !== @side_3 && @side_2 !== @side_3
     return :scalene
 end
-
+end
 class TriangleError < StandardError
    # triangle error code
  end
