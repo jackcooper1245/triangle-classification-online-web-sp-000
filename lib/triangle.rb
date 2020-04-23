4430d4d91ca00cb7bd978630c64e73b7159fb5ab
Jack Cooper <jackcooper1245@gmail.com>
2020-04-23 10:04:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,30 +1,42 @@
 class Triangle
+  # write code here\
 
-def initialize(side_1, side_2, side_3)
-@side_1 = side_1
-@side_2 = side_2
-@side_3 = side_3
+  def initialize(s1, s2, s3)
+
+  
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
+    end
+
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if @s1 == @s2 && @s1 == @s3
+      return :equilateral
+    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
 end
 
 
-#def kind
-#  if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-#    raise TriangleError
-#  elsif(@side_1 + @side_2) < @side_3 || (@side_1 + @side_3) < @side_1 || (@side_2 + @side_3) < @side_1
-#    raise TriangleError
-#  elsif (@side_1 == @side_2) && (@side_2 == @side_3)
-#    return :equilateral
-#  elsif (@side_1 == @side_2) && (@side_1 !== @side_3)
-#    return :isosceles
-#  elsif (@side_1 !== @side_2) && (@side_1 !== @side_3) && (@side_2 !== @side_3)
-#    return :scalene
-#end
-#end
-
-end
 
 class TriangleError < StandardError
   def message
-    "This is not a triangle"
+    "This is not a valid triangle."
   end
- end
+end
